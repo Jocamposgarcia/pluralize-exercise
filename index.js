cfae3cab254046418db1bf2e0caf72b01b2fb148
jocamposgarcia <jorge.andresocampo@gmail.com>
2019-05-30 20:47:44 -0400

    something

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
-
+var pluralize = require('pluralize');
 function getPluralization(word) {
     // This needs to be replaced with the code that returns a pluralized version of the input word
-    return word;
+    return pluralize(word);
+    //
 }
 
 function printPluralizedWord(word) {
